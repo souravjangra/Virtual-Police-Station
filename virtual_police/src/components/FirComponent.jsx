ac8ddbb49b7ab5baf229486676e488c13c2c799e
Sourav Jangra <imsouravjangra@gmail.com>
2020-02-04 23:37:16 +0530

    FIR pdf functionality added

--- virtual_police/src/components/FirComponent.jsx
+++ virtual_police/src/components/FirComponent.jsx
@@ -1,27 +1,121 @@
 import React, {Component} from "react";
 import HomeComponent from "./HomeComponent";
-var fs = require('fs');
-var pdf = require('html-pdf');
-var html = fs.readFileSync('l', 'utf8');
-var options = { format: 'Letter'};
+import Modal from 'react-modal';
+import axios from 'axios';
 
 class FirComponent extends Component{
 
-    state= {
-        backBtn: false,
+    componentWillMount() {
+        Modal.setAppElement(document.getElementById('root'));
+    }
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            modalIsOpen: false,
+            date: (new Date()).toString().split(' ').splice(1,3).join(' '),
+            backBtn: false,
+            name: "",
+            address: "",
+            mobile: "",
+            email: "",
+            subject: "",
+            incident: "",
+        };
+
+        this.openModal = this.openModal.bind(this);
+        this.afterOpenModal = this.afterOpenModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    openModal() {
+        this.setState({modalIsOpen: true});
+
+        // axios.post('http://localhost:8081/confirmFIR', this.state.name)
+        //     .then(res => {
+        //         console.log(res);
+        //         console.log(res.data);
+        //     })
+    }
+
+    afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        // this.subtitle.style.color = '#000';
+    }
+
+    closeModal() {
+        this.setState({modalIsOpen: false});
+    }
+
+    getDate = () => {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = mm + '/' + dd + '/' + yyyy;
+        this.setState({date: today});
     };
 
     submitFir = () => {
+        console.log(this.state.name);
+        console.log(this.state.address);
         console.log("submitted fir.");
-        pdf.create(html, options).toFile('../../fir.pdf', function (err, res) {
-            if(err) return console.log(err);
+        console.log(this.state.date);
+
+        axios({
+            method: 'post',
+            url: 'http://localhost:8081/generateFIR',
+            data: {name: this.state.name,
+                    address: this.state.address,
+                mobile: this.state.mobile,
+                email: this.state.email,
+                date: this.state.date,
+                subject: this.state.subject,
+                incident: this.state.incident
+
+            },
+            // headers: {'Content-Type': 'text/html' }
+        }).then(res => {
             console.log(res);
+            console.log(res.data);
+            this.closeModal();
         });
+        // var formData = new FormData();
+        // formData.set('name', this.state.name);
+        // axios({
+        //     method: 'post',
+        //     url: 'http://localhost:8081/generateFIR',
+        //     data: {name:this.state.name},
+        //     //headers: {'Content-Type': 'text/html' }
+        // }).then(res => {
+        //     console.log(res);
+        //     console.log(res.data);
+        //     this.closeModal()
+        //
+        // });
+        // axios.post("http://localhost:8081/generateFIR")
+        //     .then(res => {
+        //         console.log(res);
+        //         console.log(res.data);
+        //     })
     };
 
     render() {
         const styles = {
             marginTop: '50px',
+        };
+
+        const customStyles = {
+            content : {
+                top                   : '25%',
+                left                  : '50%',
+                right                 : '50%',
+                bottom                : 'auto',
+                marginRight           : '-50%',
+                transform             : 'translate(-50%, -50%)'
+            }
         };
 
         return (
@@ -32,11 +126,31 @@ class FirComponent extends Component{
                         this.setState({backBtn: true})
                     }} id="backBtn" className="btn btn-info mt-4">Back</button>
                     <h4 style={{marginTop: '20px'}}>Fill up the details below: </h4>
-                    <form>
+                    {/*<button onClick={this.openModal} className="btn btn-primary">Open Modal</button>*/}
+                    <Modal
+                        isOpen={this.state.modalIsOpen}
+                        onAfterOpen={this.afterOpenModal}
+                        onRequestClose={this.closeModal}
+                        style={customStyles}
+                        contentLabel="Confirm FIR Modal"
+                    >
+
+                        {/*<h2 ref={subtitle => this.subtitle = subtitle}>Submit*/}
+                        {/*</h2>*/}
+                        <div>Are you sure you want to Submit?
+                            Information Entered once cannot be changed.
+                            Please check the entered information carefully.</div>
+                        <button className="btn btn-secondary btn-sm m-2" onClick={this.closeModal}>close</button>
+                        <button className="btn btn-success btn-sm ml-2" onClick={this.submitFir}>Submit</button>
+                    </Modal>
+
+                    <form onSubmit={this.openModal}>
                         <div className="form-group">
                             <label>
                                 Name:<br/>
-                                <input type="text" name="name" className="form-control" placeholder="Applicant’s Name or Father’s/Husband’s Name" size="80"/>
+                                <input id="name" type="text" name="name" className="form-control" placeholder="Applicant’s Name or Father’s/Husband’s Name" size="80"
+                                value={this.state.name} onChange={(e) => {this.setState({name: e.currentTarget.value})}}
+                                />
                             </label>
                             <br/>
 
@@ -56,18 +170,22 @@ class FirComponent extends Component{
                             <label>
                                 Details of the Incident:
                             </label><br/>
-                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"/>
+                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"
+                                      value={this.state.incident} onChange={(e) => {this.setState( {incident: e.currentTarget.value})}}
+                            />
                             <br/>
 
                             <label>
                                 Phone No:<br/>
-                                <input type="text" name="phone" className="form-control" placeholder="Applicant’s Phone No." size="80"/>
+                                <input value={this.state.mobile} onChange={(e) => {this.setState( {mobile: e.currentTarget.value})}} type="text" name="phone" className="form-control" placeholder="Applicant’s Phone No." size="80"/>
                             </label>
                             <br/>
 
                             <label>
                                 Address:<br/>
-                                <input type="text" name="address" className="form-control" placeholder="Applicant’s Contact Address" size="80"/>
+                                <input value={this.state.address}
+                                       onChange={(e) => {this.setState({address: e.currentTarget.value})}}
+                                       type="text" name="address" className="form-control" placeholder="Applicant’s Contact Address" size="80"/>
                             </label>
                             <br/>
 
@@ -91,7 +209,10 @@ class FirComponent extends Component{
 
                             <label>
                                 Email Address:<br/>
-                                <input type="text" name="email" className="form-control" placeholder="Applicant’s Email Address(If Any)" size="80"/>
+                                <input
+                                    value={this.state.email}
+                                    onChange={(e) => {this.setState({email: e.currentTarget.value})}}
+                                    type="text" name="email" className="form-control" placeholder="Applicant’s Email Address(If Any)" size="80"/>
                             </label>
                             <br/>
 
@@ -107,10 +228,11 @@ class FirComponent extends Component{
                             </div>
 
                         </div>
-                        <div className="text-center mb-5">
-                        <button onClick={this.submitFir} type="submit" className="btn btn-primary" formAction="">Submit</button>
-                        </div>
+
                     </form>
+                    <div className="text-center mb-5">
+                        <button onClick={this.openModal} type="submit" className="btn btn-primary" >Submit</button>
+                    </div>
                 </div>
                 }
             </React.Fragment>
---
+++ virtual_police/src/server.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var app = express();
+var { parse }=require('node-html-parser');
+var fs = require('fs');
+var pdf = require('html-pdf');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var htmlFileToBeChanged ;
+// sql stuff
+const mysql= require('mysql');
+
+const connection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'wrong-password',
+    database:'testDb',
+    insecureAuth:'true'
+});
+
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    res.send('Its backend for the app, see the documentation for more details');
+})
+
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
+
+app.post('/confirmFIR',urlencodedParser,function(req,res){
+    let html = fs.readFileSync('./fir.html', 'utf8');
+    let options = { format: 'Letter' };
+    let name=req.body.name;
+    // let email=req.body.email;
+    console.log(" name is "+name);
+    htmlFileToBeChanged = parse(html);
+    //
+    // // setting feilds in html
+    htmlFileToBeChanged.querySelector("#person_name").setAttribute('placeholder',name);
+    // htmlFileToBeChanged.querySelector('#exampleInputEmail').setAttribute('placeholder',email);
+    res.send("confirm fir");
+    res.send(htmlFileToBeChanged.toString());
+})
+
+app.post('/generateFIR',urlencodedParser,function(req,res){
+    let html = fs.readFileSync('./fir.html', 'utf8');
+    let options = { format: 'Letter' };
+
+    let name = req.body.name;
+    let address = req.body.address;
+    let mobile = req.body.mobile;
+    let email = req.body.email;
+    let date = req.body.date;
+    let incident = req.body.incident;
+
+    htmlFileToBeChanged = parse(html);
+    htmlFileToBeChanged.querySelector("#person_name").setAttribute('value',name);
+    htmlFileToBeChanged.querySelector("#person_address").setAttribute('value',address);
+    htmlFileToBeChanged.querySelector("#person_mobile_no").setAttribute('value',mobile);
+    htmlFileToBeChanged.querySelector("#person_email_id").setAttribute('value',email);
+    htmlFileToBeChanged.querySelector("#date").setAttribute('value',date);
+    htmlFileToBeChanged.querySelector("#incident").setAttribute('value',incident);
+
+    console.log("name is " + req.body.name);
+
+    pdf.create(htmlFileToBeChanged.toString(), options).toFile('./pdfForm.pdf', function(err, res) {
+        if (err)
+            return console.log(err);
+        else{
+            console.log(res);
+        }
+    });
+    // res.send(htmlFileToBeChanged.toString());
+    res.send(req.body);
+});
+
+app.get('/getAll',function(request, response){
+    const select_all_command="select * from user";
+    connection.query(select_all_command,(err,results)=>{
+        if(err){
+            return response.send(err);
+        }
+        else{
+            return response.json({data:results})
+        }
+    })
+});
+
+app.post('/signUp',function(request,response){
+    // first name, last name, email and mobile number and addhar
+    const {firstName,lastName,email,mobileNumber,aadhar} =request.body;
+    postParams="\""+firstName+"\", \""+lastName+"\", \""+email+"\", \""+mobileNumber+"\", \""+aadhar+"\"";
+    const insert_query="Insert into user (firstName, lastName, email, mobileNumber, aadhar) values ("+ postParams+")";
+    console.log("query command is "+insert_query);
+    connection.query(insert_query,(error,results)=>{
+        if(error){
+            return response.send(error);
+        }else{
+            return response.json({data:request.body});
+        }
+    });
+    // response.json({data:"some message "});
+})
